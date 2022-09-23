8d29e000b4ecb2768ec1ecf87d8a827a15f34eb8
Walker Hebert <walkerhebert@yahoo.com>
2022-09-23 18:10:21 -0500

    Real-Final-Commit

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,3 +18,22 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+test('Choices display when Draw button is clicked', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(300)
+    const choicesDiv = await driver.findElement(By.id('choices'))
+    expect(choicesDiv).not.toBe('')
+})
+
+test('Selecting a bot displays in our new div', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(300)
+    await driver.findElement(By.xpath('(//*[text()="Add to Duo"])[1]')).click()
+
+    const playerDuoDiv = await driver.findElement(By.id('player-duo'))
+    const displayed = await playerDuoDiv.isDisplayed()
+
+    expect(displayed).toBe(true)
+
+})
--- functions.test.ts
+++ functions.test.ts
@@ -4,8 +4,25 @@ describe('shuffleArray should', () => {
     test('Check that shuffleArray returns an array', () => {
         expect(shuffleArray.length).toBe(1)
     })
-    test('Check that all the same items are in the array', () => {
-        const randoArray = [2, 4, 6, 8, 10]
-        expect(shuffleArray(randoArray).length).toEqual(randoArray.length)
-    })
+//     test('Check that all the same items are in the array', () => {
+//         const randoArray = [2, 4, 6, 8, 10]
+//         expect(shuffleArray(randoArray).length).toEqual(randoArray.length)
+//     })
+// })
+
+test('Check that all the same items are in the array', () => {
+    let randoArray = [2, 4, 6, 8, 10]
+    let result = shuffleArray(randoArray)
+    
+
+    let myVar = true
+
+    for( let i = 0; i < result.length; i++){
+        if(randoArray.includes(result[i]) === false){ 
+            myVar = false
+            return
+    }
+}
+    expect(myVar).toBe(true)
+})
 })
